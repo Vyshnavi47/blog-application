fce6b080a1e907116af5053814f020dfc93c33a2
Vyshnavi47 <131758160+Vyshnavi47@users.noreply.github.com>
2025-01-31 23:16:09 +0530

    Update Routes.js

--- Backend/routers/Routes.js
+++ Backend/routers/Routes.js
@@ -1,139 +1,134 @@
 const express = require('express');
-Routers = express.Router();
+const Routers = express.Router();
 const mongoose = require('mongoose');
-const Blog = require("../Model/RegisterDB")
-const bcrypt = require('bcrypt')
-const jwt = require("jsonwebtoken")
-const Validation = require("../validation/verfiy")
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads' })
-const fs = require('fs')
+const Blog = require("../Model/RegisterDB");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const Validation = require("../validation/verfiy");
+const multer = require('multer');
+const fs = require('fs');
 const BlogPosts = require("../Model/Posts");
+const path = require('path');
 
+// Configure multer to use the /tmp directory for file uploads in vercel
+const upload = multer({
+  dest: '/tmp' 
+});
 
 // ========================================================================
 // -------------------login a User ------------------------------------------
-
 Routers.route('/login').post(
-    async (req,res)=>{
-        const {email,pass} = req.body;
-        if(!email || !pass){
-            res.send("All fields are manditotary ...");
-        }
-        else{
-            const Login = await Blog.findOne({email});
-            if(!Login){
+    async (req, res) => {
+        const { email, pass } = req.body;
+        if (!email || !pass) {
+            res.send("All fields are mandatory ...");
+        } else {
+            const Login = await Blog.findOne({ email });
+            if (!Login) {
                 res.send("no user found");
-            }else{
-                // --comp pass---
-                if(await bcrypt.compare(pass,Login.pass)){
+            } else {
+                // --compare pass---
+                if (await bcrypt.compare(pass, Login.pass)) {
                     const KEY = "@#$%^&*()";
                     const JsonToken = jwt.sign({
-                        user:{
-                            id:Login.id,
-                            email:Login.email
+                        user: {
+                            id: Login.id,
+                            email: Login.email
                         }
-                    },KEY,{expiresIn:"1d"})
+                    }, KEY, { expiresIn: "1d" })
                     res.status(200).send(JsonToken)
-                }
-                else{
+                } else {
                     res.send("wrong pass")
                 }
             }
         }
-})
+    })
 
 // ============================================================================================
 // --------------------Register user Route ---------------------------------
 Routers.route("/register").post(
-   async (req,res)=>{
-    const {first,last,email,pass} = req.body;
-    if(!first || !last || !email || !pass){
-        res.send("All fields are required")
-    }else{
-        const Find  = await Blog.findOne({email})
-        if(Find){
-            res.send("user already exists")
-        }
-        else{
-
-            // ------------Hash password----------
-            const HashPass = await bcrypt.hash(pass,10)
-        const Posting =await Blog.insertMany({
-            first:first,
-            last:last,
-            email:email,
-            pass:HashPass
-        })
-        if(Posting){
-            res.status(200).send("Data posted sucessfully in db...")
-        }
-        else{
-            res.send("failed..")
+    async (req, res) => {
+        const { first, last, email, pass } = req.body;
+        if (!first || !last || !email || !pass) {
+            res.send("All fields are required")
+        } else {
+            const Find = await Blog.findOne({ email })
+            if (Find) {
+                res.send("user already exists")
+            } else {
+                // ------------Hash password----------
+                const HashPass = await bcrypt.hash(pass, 10)
+                const Posting = await Blog.insertMany({
+                    first: first,
+                    last: last,
+                    email: email,
+                    pass: HashPass
+                })
+                if (Posting) {
+                    res.status(200).send("Data posted successfully in db...")
+                } else {
+                    res.send("failed..")
+                }
+            }
         }
     }
-}}
 )
 // ===============================================================
 // -------------------Posts-----------------------------
-
 Routers.route("/posts").get(
-   async (req,res)=>{
-    const BlogsData =await BlogPosts.find({})
-    res.send(BlogsData)
-})
+    async (req, res) => {
+        const BlogsData = await BlogPosts.find({})
+        res.send(BlogsData)
+    })
 
 // ===============================================================
 // ----------------Blogs---------------------------------
-
-
-Routers.route("/blogs").post(upload.single('file'),async (req, res) => {
-   if(!req.file){
-    res.send("error no file")
-   }
-   const {originalname,path} = req.file;
-   const parts = originalname.split('.');
-   const ext = parts[parts.length -1 ]
-   const newPath = path+ '.'+ext 
-   fs.renameSync(path,newPath);
-    const {heading,summary,text,jwtToken} = req.body;
-    const decode = jwt.verify(jwtToken,"@#$%^&*()");
-    datas = decode.user
-    const UserName = await Blog.findOne({email:datas.email})
+Routers.route("/blogs").post(upload.single('file'), async (req, res) => {
+    if (!req.file) {
+        res.send("error no file")
+    }
+    const { originalname, path } = req.file;
+    const ext = path.extname(originalname); 
+    const newPath = path + ext; 
+    fs.renameSync(path, newPath);
+    
+    const { heading, summary, text, jwtToken } = req.body;
+    const decode = jwt.verify(jwtToken, "@#$%^&*()");
+    const datas = decode.user;
+    const UserName = await Blog.findOne({ email: datas.email });
     const Postblog = await BlogPosts.insertMany({
-            first:UserName.first,
-            last:UserName.last,
-            email:UserName.email,
-            heading:heading,
-            summary:summary,
-            image:newPath,
-            text:text
+        first: UserName.first,
+        last: UserName.last,
+        email: UserName.email,
+        heading: heading,
+        summary: summary,
+        image: newPath, 
+        text: text
     })
-    res.status(200).send("posted Blog into DB.") 
+    res.status(200).send("posted Blog into DB.")
 });
 
-
 // ===============================================================
-// -------------delete all bolgs -----------------------
+// -------------delete all blogs -----------------------
 Routers.route('/delete').post(async (req, res) => {
     try {
-      const id = req.body.id;        
-      if (!id) {
-        return res.status(400).json({ error: 'Invalid ID provided' });
-      }
-      const result = await BlogPosts.findByIdAndRemove(id);
+        const id = req.body.id;
+        if (!id) {
+            return res.status(400).json({ error: 'Invalid ID provided' });
+        }
+        const result = await BlogPosts.findByIdAndRemove(id);
         if (!result) {
-        return res.status(404).json({ error: 'Blog post not found' });
-      }
-    res.status(200).json({ message: 'Blog post deleted successfully' });
+            return res.status(404).json({ error: 'Blog post not found' });
+        }
+        res.status(200).json({ message: 'Blog post deleted successfully' });
     } catch (error) {
-      console.error('Error deleting blog post:', error);
-      res.status(500).json({ error: 'Internal server error' });
+        console.error('Error deleting blog post:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
-  });
+});
+
 // ===============================================================
 // ----------------count Posts ------------------
-
 Routers.route('/count').post(
     async (req, res) => {
         try {
@@ -150,76 +145,70 @@ Routers.route('/count').post(
     }
 );
 
-
-
 // ===============================================================
 // -----------Validate-------------------
-Routers.post('/getsingleuserdata', async (req, res) => {   
-        const token = req.header('x-token'); 
-        if (!token) {
-            return res.status(403).send("No token found"); 
-        } 
-        else {       
-                const decode = jwt.verify(token, "@#$%^&*()");
-                if (!decode) {
-                    return res.send("no decode"); 
-                }
-                const USERDATA = decode.user.email;
-                const getAbout = await Blog.findOne({ email: USERDATA });
-                if (getAbout) {
-                    return res.send(getAbout); 
-                } else {
-                    return res.send("no user"); 
-                }
-
-            
-        
-    } 
+Routers.post('/getsingleuserdata', async (req, res) => {
+    const token = req.header('x-token');
+    if (!token) {
+        return res.status(403).send("No token found");
+    }
+    else {
+        const decode = jwt.verify(token, "@#$%^&*()");
+        if (!decode) {
+            return res.send("no decode");
+        }
+        const USERDATA = decode.user.email;
+        const getAbout = await Blog.findOne({ email: USERDATA });
+        if (getAbout) {
+            return res.send(getAbout);
+        } else {
+            return res.send("no user");
+        }
+    }
 });
-// =====================================================================================
-// -----------------------Update the posted blog route---------------------------------
 
+
+// -----------------------Update the posted blog route---------------------------------
 Routers.route('/updateTheBlog/:id').put(upload.single('file'), async (req, res) => {
     try {
-      let newPath = null;
-  
-      if (req.file) {
-        const { originalname, path } = req.file;
-        const parts = originalname.split('.');
-        const ext = parts[parts.length - 1];
-        newPath = path + '.' + ext;
-        fs.renameSync(path, newPath);
-      }
-  
-      const { heading, summary, text } = req.body;
-  
-      if (!heading && !summary && !text) {
-        return res.status(200).send("Fields are empty");
-      }
-  
-      const updateData = {};
+        let newPath = null;
 
-      if (heading) {
-        updateData.heading = heading;
-      }
-      if (summary) {
-        updateData.summary = summary;
-      }
-      if (text) {
-        updateData.text = text;
-      }
-      if (newPath) {
-        updateData.image = newPath;
-      }
-      const PosiId = req.params.id
-      const result = await BlogPosts.updateOne({ _id: PosiId }, updateData)
-    
-      res.status(200).send("Data updated successfully");
+        if (req.file) {
+            const { originalname, path } = req.file;
+            const ext = path.extname(originalname); 
+            newPath = path + ext;
+            fs.renameSync(path, newPath); 
+        }
+
+        const { heading, summary, text } = req.body;
+
+        if (!heading && !summary && !text) {
+            return res.status(200).send("Fields are empty");
+        }
+
+        const updateData = {};
+
+        if (heading) {
+            updateData.heading = heading;
+        }
+        if (summary) {
+            updateData.summary = summary;
+        }
+        if (text) {
+            updateData.text = text;
+        }
+        if (newPath) {
+            updateData.image = newPath; 
+        }
+
+        const PosiId = req.params.id;
+        const result = await BlogPosts.updateOne({ _id: PosiId }, updateData);
+
+        res.status(200).send("Data updated successfully");
     } catch (error) {
-      console.error("Error updating data:", error);
-      res.status(200).send("Internal Server Error");
+        console.error("Error updating data:", error);
+        res.status(200).send("Internal Server Error");
     }
-  });
+});
 
-
-module.exports = Routers
+module.exports = Routers;
